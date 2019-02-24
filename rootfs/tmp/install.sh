433214e75ebbeb4fd23a6489aaf34fefcf02dc9c
ZeroC0D3 Engineer <dfdenni0912@gmail.com>
2019-02-25 02:38:46 +0700

    Change port, add bundle installation

--- rootfs/tmp/install.sh
+++ rootfs/tmp/install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
-### Path Ruby RBENV ###
+### Path RBENV ###
 export RBENV_ROOT="$HOME/.rbenv"
-export INSTALL_VERSION=${RUBY_VERSION:-2.6.1}
+export INSTALL_VERSION=${RUBY_VERSION:-2.4.5}
 
 git clone https://github.com/rbenv/rbenv.git $RBENV_ROOT
 git clone https://github.com/rbenv/ruby-build.git $RBENV_ROOT/plugins/ruby-build
 
-# Install Ruby with rbenv (default)
+#-----------------------------------------------------------------------------
+# Install Ruby with RBENV
 #-----------------------------------------------------------------------------
 $RBENV_ROOT/bin/rbenv install $INSTALL_VERSION
 $RBENV_ROOT/bin/rbenv global $INSTALL_VERSION
@@ -15,3 +16,6 @@ $RBENV_ROOT/bin/rbenv rehash
 
 ln -sf $RBENV_ROOT/bin/rbenv /usr/local/bin/rbenv
 ln -sf $RBENV_ROOT/shims/ruby /usr/local/bin/ruby
+ln -sf $RBENV_ROOT/shims/gem /usr/local/bin/gem
+
+gem install bundler
