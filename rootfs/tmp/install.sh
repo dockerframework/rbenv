f0da6fd59b6956992639356a4f36ff1fc5a8f986
ZeroC0D3 Engineer <dfdenni0912@gmail.com>
2019-02-24 14:29:42 +0700

    First commit to master branch

---
+++ rootfs/tmp/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+### Path Ruby RBENV ###
+export RBENV_ROOT="$HOME/.rbenv"
+export INSTALL_VERSION=${RUBY_VERSION:-2.4.5}
+
+git clone https://github.com/rbenv/rbenv.git $RBENV_ROOT
+git clone https://github.com/rbenv/ruby-build.git $RBENV_ROOT/plugins/ruby-build
+
+# Install Ruby with rbenv (default)
+#-----------------------------------------------------------------------------
+$RBENV_ROOT/bin/rbenv install $INSTALL_VERSION
+$RBENV_ROOT/bin/rbenv global $INSTALL_VERSION
+$RBENV_ROOT/bin/rbenv rehash
+
+ln -sf $RBENV_ROOT/bin/rbenv /usr/local/bin/rbenv
+ln -sf $RBENV_ROOT/shims/ruby /usr/local/bin/ruby
